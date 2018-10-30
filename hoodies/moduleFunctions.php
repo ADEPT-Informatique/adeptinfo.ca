961448500f2cf3e96967f6cf634eb8d6073e49d7
Olivier Brassard <brassard.oli@gmail.com>
2018-10-30 16:44:59 -0400

    Patch d'un bug  sql sur la page d'admin des hoodies

--- hoodies/moduleFunctions.php
+++ hoodies/moduleFunctions.php
@@ -194,7 +194,7 @@ function UpdateReservation($reservationID, $depot, $size, $isRecupered)
             'ID' => $reservationID,
             'depot' => $depot,
             'size' => $size,
-            'recup' => $isRecupered
+            'recup' => $isRecupered ? 1:0
         ));
 
         return true;
@@ -215,7 +215,7 @@ function SaveInHistory($datas)
             'nom' => $datas['Nom'],
             'type' => $datas['Type'],
             'depot' => $datas['Depot'],
-            'recup' => $datas['Recup']
+            'recup' => $datas['Recup'] ? 1:0
         ));
 
         return true;
