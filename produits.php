d793be28b86f0c2c971768d603048b32d3094175
Matei Martin <mat4school@gmail.com>
2019-01-21 22:42:04 -0500

    Update produits.
    -Mis a jour la liste des produits vers la version détaillée.

--- produits.php
+++ produits.php
@@ -1,5 +1,5 @@
 <?php 
-require_once("model/bd_articles.php");
+require_once("admin/model/bd_articles.php");
 include("includes/header.php");
 ?>
 
@@ -60,12 +60,11 @@ include("includes/header.php");
                             <table class="table table-striped">
                                 <thead>
                                     <tr>
-                                        <th scope="col">Article</th>
-                                        <th scope="col"></th>
-                                        <th scope="col">Prix</th>
-                                        <th scope="col">Description</th>
-                                        <th scope="col"></th>
-                                        <th scope="col"></th>
+                                                <th scope="col">Image</th>
+                                                <th scope="col">Article</th>
+                                                <th scope="col">Prix</th>
+                                                <th scope="col">Inventaire</th>
+                                                <th scope="col">Description</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -73,17 +72,49 @@ include("includes/header.php");
                                         $tabAllArticles = getAllArticles();
                                         foreach ($tabAllArticles as $Article)
                                     { ?>
-                                    <tr>
-                                        <td colspan="2">
-                                            <?php echo $Article['nom']; ?>
-                                        </td>
-                                        <td>
-                                            <?php echo $Article['cout']; ?>
-                                        </td>
-                                        <td colspan="3">
-                                            <?php echo $Article['info']; ?>
-                                        </td>
-                                    </tr>
+                                    <tr class="clickable" data-toggle="collapse" id="row<?php echo $Article['articleID'];?>" data-target=".row<?php echo $Article['articleID'];?>">
+                                                <td >
+                                                    <img height="100" width="100" src="<?php echo "../../img/epicerie/".$Article['articleID'].".jpg"; ?>" />
+                                                </td>
+                                                <td >
+                                                    <?php echo $Article['nom']; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $Article['cout']; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $Article['qtyCourant']; ?>
+                                                </td>
+                                                <td >
+                                                    <?php echo $Article['info']; ?>
+                                                </td>
+                                            </tr><!--
+                                            <tr class="collapse row<?php echo $Article['articleID'];?>">
+                                                <form method="post" action="../controller/ArticleEdit.php">
+                                                    <td >
+                                                        <div class="custom-file">
+                                                            <label class="custom-file-label" for="customFile">IMG</label>
+                                                            <input type="file" class="custom-file-input" style="width:2rem;" id="file" name="file">
+                                                        </div>
+                                                    </td>
+                                                    <td >
+                                                        <input type="hidden" value="<?php echo $Article['articleID']?>" name="id" class="form-control" id="id" />
+                                                        <input type="text" value="<?php echo $Article['nom']?>" name="nom" class="form-control" id="nom" />
+                                                    </td>
+                                                    <td>
+                                                    <input type="number" value="<?php echo $Article['cout']?>" name="cout" class="form-control" id="cout" step=".01"/>
+                                                    </td>
+                                                    <td>
+                                                    <input type="number" value="<?php echo $Article['qtyCourant']?>" name="qty" class="form-control" id="qty"  />
+                                                    </td>
+                                                    <td >
+                                                    <input type="text" value="<?php echo $Article['info']?>" name="desc" class="form-control" id="desc"  />
+                                                    </td>
+                                                    <td >
+                                                        <input class="btn btn-primary" type="submit" name="submit" value="Modifier"/><br><br>
+                                                    </td>
+                                                </form>
+                                            </tr>-->
                                     <?php } ?>
                                 </tbody>
                             </table>
